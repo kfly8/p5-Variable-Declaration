65a55e5c6d3287658ffa74c637555568f01b03a1
Kenta, Kobayashi <kentafly88@gmail.com>
2018-02-15 15:07:01 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+PerlX::Declare - It's new $module
+
+# SYNOPSIS
+
+    use PerlX::Declare;
+
+# DESCRIPTION
+
+PerlX::Declare is ...
+
+# LICENSE
+
+Copyright (C) Kenta, Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kenta, Kobayashi <kentafly88@gmail.com>
---
+++ lib/PerlX/Declare.pm
@@ -0,0 +1,39 @@
+package PerlX::Declare;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+PerlX::Declare - It's new $module
+
+=head1 SYNOPSIS
+
+    use PerlX::Declare;
+
+=head1 DESCRIPTION
+
+PerlX::Declare is ...
+
+=head1 LICENSE
+
+Copyright (C) Kenta, Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kenta, Kobayashi E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
